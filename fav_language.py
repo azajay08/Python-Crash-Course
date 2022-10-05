f655225f65e535dd5c52840a94ca1287c65fbbcb
Aaron Jones <aaronjones@Aarons-MacBook-Pro.local>
2022-10-06 01:57:36 +0300

    new work

--- fav_language.py
+++ fav_language.py
@@ -23,3 +23,25 @@ fav_language = '|python|'
 new_language = '         |java|         '
 lan_language = '|ruby|'
 print(fav_language, new_language.strip(), lan_language)
+
+# dictionary
+
+fav_languages = {
+	'aaron': 'python',
+	'elliot': 'ruby',
+	'miro': 'c',
+	'lotta': 'ruby',
+	}
+
+language = fav_languages['lotta'].title()
+print(f"\n\nLotta's favourite language is {language}.\n\n\n")
+
+
+#########
+
+for name, language in fav_languages.items():
+	print(f"\n{name.title()}'s favourite language is {language.title()}.")
+
+print('\n\n')
+for name in fav_languages.keys():
+	print(name.title())
---
+++ user.py
@@ -0,0 +1,12 @@
+from multiprocessing.sharedctypes import Value
+
+
+user_o = {
+	'username': 'azajay08',
+	'first name': 'aaron',
+	'last name': 'jones',
+}
+
+for key, value in user_o.items():
+	print(f"\nKey: {key}")
+	print(f"Value: {value.title()}")
