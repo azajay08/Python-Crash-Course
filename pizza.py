4677405616efaf22a80d44afba9885527a94c197
Aaron Jones <aaron.p.j@hotmail.com>
2022-10-08 00:53:06 +0300

    functions multi arguments

--- pizza.py
+++ pizza.py
@@ -8,4 +8,15 @@ print(f"You ordered a {pizza['crust']}-crust pizza "
 
 for topping in pizza['toppings']:
 	print(f"\t{topping}")
+
+# multi arguments using asterisk *
+def make_pizza(*toppings):
+	"""can call multiple arguments"""
+	print("\nMaking pizza with the following toppings:")
+	for topping in toppings:
+		print(f"- {topping}")
+
+make_pizza('cheese', 'tomato')
+
+make_pizza('olives', 'cheese', 'pepperoni')
 	
